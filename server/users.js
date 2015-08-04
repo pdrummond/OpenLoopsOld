cd14691a92d011983902d7c269ce9b74a8034a58
Paul Drummond <paul.drummond@iode.co.uk>
2015-08-04 18:18:41 +0100

    Fixed 'user has no password set' bug

--- server/users.js
+++ server/users.js
@@ -1,5 +1,9 @@
-Accounts.onCreateUser(function(user) {	
+Accounts.onCreateUser(function(options, user) {	
 	console.log("newUser: " + JSON.stringify(user));
-	user.profileImage = Gravatar.imageUrl(user.email, {size: 34,default: 'retro'});
+	user.profileImage = Gravatar.imageUrl(user.emails[0].address, {size: 34,default: 'retro'});
+	// We still want the default hook's 'profile' behavior.
+	if (options.profile) {
+		user.profile = options.profile;
+	}
 	return user;
 });
